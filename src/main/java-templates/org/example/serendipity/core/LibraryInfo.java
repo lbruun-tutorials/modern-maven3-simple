05e150977529b3a262f3d94de6c3b9d383f88b2c
Lars Bruun-Hansen <lbruun@apache.org>
2025-03-21 09:17:44 +0100

    Initial

--- src/main/java-templates/org/example/serendipity/core/LibraryInfo.java
+++ src/main/java-templates/org/example/serendipity/core/LibraryInfo.java
@@ -1,102 +1,100 @@
-package org.example.core;
+package org.example.serendipity.core;
 
 import java.time.Instant;
-import java.util.Objects;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
-/** Static information about this library */
+/** Static information about this library. */
 public class LibraryInfo {
 
-    // Values '${...}' will be replaced by Maven at build time
-    private final String version = "${project.version}";
-    private final String buildTimeStr = "${git.build.time}"; // must be in RFC-3339 format
-    private final String commitTimeStr  = "${git.commit.time}"; // must be in RFC-3339 format
-    private final String commitId = "${git.commit.id}";
-    private final String projectUrl  = "${project.url}";
-    private final String licenseName  = "${project.licenses[0].name}";
-    private final String licenseUrl  = "${project.licenses[0].url}";
-    private static final LibraryInfo INSTANCE = new LibraryInfo();
+    // This class is meant to be used with the Template Maven Plugin
+    // (https://www.mojohaus.org/templating-maven-plugin/)
+    //
+    // The plugin will replace the ${...} values at build time with values from the Maven session.
+
+    private static final Pattern REPLACEMENT_VAR_PATTERN =
+            Pattern.compile("\\$\\{[A-Za-z0-9.\\-_\\[\\]]+}");
 
     private LibraryInfo() {
-    }
-
-    public static LibraryInfo get() {
-        return INSTANCE;
     }
 
     /**
      * Version of library.
      *
-     * @return version as a string or empty string if unknown, never {@code null}.
+     * <p>
+     * Returns empty string if unknown, never {@code null}.
      */
-    public String version() {
-        return fixUnresolved(version);
-    }
+    public static String version = fixUnresolved("${project.version}");
 
     /**
      * Time the library was build/compiled.
      *
-     * @return time of build. Empty if no build time information is available.
+     * <p>
+     * Returns empty if no build time information is available.
      */
-    public Optional<Instant> buildTime() {
-        String s = fixUnresolved(buildTimeStr);
-        if (s != null && (!s.isEmpty())) {
-            return Optional.of(Instant.parse(s));
-        }
-        return Optional.empty();
-    }
+    // Note: This comes from 'maven.build.timestamp' but Maven does not make
+    // this variable available for property substition directly. Instead, we
+    // have to create a user property for it ('buildTime'). And then use that as an alias.
+    // See the POM for more information.
+    public static Optional<Instant> buildTime = getInstant("${buildTime}"); // must be in RFC-3339 format
+
 
     /**
-     * Time of the commit to version control system (VCS) for the commit from which this library was build.
+     * Time of the commit to version control system (VCS) for the commit
+     * from which this library was build.
      *
-     * @return time of commit or empty if unknown
+     * <p>
+     * Returns empty if unknown/unavailable.
      */
-    public Optional<Instant> vcsCommitTime() {
-        String s = fixUnresolved(commitTimeStr);
-        if (s != null && (!s.isEmpty())) {
-            return Optional.of(Instant.parse(s));
-        }
-        return Optional.empty();
-    }
+    public static Optional<Instant> vcsCommitTime = getInstant("${git.commit.time}"); // must be in RFC-3339 format
 
     /**
-     * Commit identifier in the version control system (VCS) for the commit from which this library was
-     * build. A commit identifier is typically an SHA-1 value.
+     * Commit identifier in the version control system (VCS) for the commit
+     * from which this library was. A commit identifier is typically an SHA-1 value.
      *
      * <p>Also sometimes referred to as a <i>commit reference</i>.
      *
-     * @return commit id or empty string if unknown, never {@code null}.
+     * <p>
+     * Returns empty string if unknown/unavailable, never {@code null}.
      */
-    public String vcsCommitId() {
-        return fixUnresolved(commitId);
-    }
+    public static String vcsCommitId = fixUnresolved("${git.commit.id}");
 
     /**
-     * URL which is the home page for the project which produced this library.
+     * URL which allows to browse the exact version of the source code
+     * that produced this library. This represents a view of the source code
+     * at the time this library was build.
      *
-     * @return URL string for the project's home page or empty string if unknown, never {@code null}.
+     * <p>
+     * For example: {@code 'https://github.com/foo/bar/tree/3.0.1'}
+     * <p>
+     * Returns empty string if unknown/unavailable, never {@code null}.
      */
-    public String projectUrl() {
-        return fixUnresolved(projectUrl);
-    }
+    public static String vcsUrl = fixUnresolved("${scm.url}");
 
     /**
-     * Name of license which applies to this library. For example: {@code 'The Apache License, Version 2.0'}.
-     * @return license name or empty string if unknown or no license, never {@code null}.
-     * @see #licenseUrl()
+     * URL for the home page for the project which produced this library.
+     * <p>
+     * Returns empty string if unknown, never {@code null}.
      */
-    public String licenseName() {
-        return fixUnresolved(licenseName);
-    }
+    public static String projectUrl = fixUnresolved("${project.url}");
 
     /**
-     * URL from where the full license text can be retrieved for the license which applies to this library. For example: {@code 'https://www.apache.org/licenses/LICENSE-2.0.txt'}.
-     * @return license URL or empty string if unknown or no license, never {@code null}.
-     * @see #licenseName()
+     * Name of license which applies to this library.
+     * For example: {@code 'The Apache License, Version 2.0'}.
+     * <p>
+     * Returns empty string if unknown or no license, never {@code null}.
+     * @see #licenseUrl
      */
-    public String licenseUrl() {
-        return fixUnresolved(licenseUrl);
-    }
+    public static String licenseName = fixUnresolved("${project.licenses[0].name}");
+
+    /**
+     * URL from where the full license text can be retrieved for the license which applies to this library.
+     * For example: {@code 'https://www.apache.org/licenses/LICENSE-2.0.txt'}.
+     * <p>
+     * Returns empty string if unknown or no license, never {@code null}.
+     * @see #licenseName
+     */
+    public static String licenseUrl = fixUnresolved("${project.licenses[0].url");
 
 
     /**
@@ -106,10 +104,19 @@ public class LibraryInfo {
      * Therefore, you may want to skip this logic and deal with it as a runtime failure
      * instead? Or simply not deal with it?
      */
-    private String fixUnresolved(String str) {
+    private static String fixUnresolved(String str) {
         if (str == null || str.isEmpty()) {
             return str;
         }
-        return str.replaceAll("\\$\\{[A-Z,a-z]+}", "");
+        return REPLACEMENT_VAR_PATTERN
+                .matcher(str).replaceAll("");
+    }
+
+    private static Optional<Instant> getInstant(String str) {
+        String s = fixUnresolved(str);
+        if (s == null || (s.isEmpty())) {
+            return Optional.empty();
+        }
+        return Optional.of(Instant.parse(s));
     }
 }
